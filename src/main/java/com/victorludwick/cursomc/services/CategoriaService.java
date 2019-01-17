eeacbb1794f453dc390b8d6f2b71d930249f9fd6
Victor Ludwick <victor_ludwick@hotmail.com.br>
2019-01-17 03:23:58 -0200

    Ajustes no endpoint /categorias/{id}

--- src/main/java/com/victorludwick/cursomc/services/CategoriaService.java
+++ src/main/java/com/victorludwick/cursomc/services/CategoriaService.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Service;
 
 import com.victorludwick.cursomc.domain.Categoria;
 import com.victorludwick.cursomc.repositories.CategoriaRepository;
+import com.victorludwick.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
@@ -14,6 +15,9 @@ public class CategoriaService {
 	
 	public Categoria buscar(Integer id) {
 		Categoria obj = repo.findOne(id);
+		if(obj == null) {
+			throw new ObjectNotFoundException("Objeto não encontrado! Id: " + id + ", Tipo " + Categoria.class.getName());
+		}
 		return obj;
 	}
 
