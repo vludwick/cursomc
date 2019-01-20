cdf0e0a78a5a271f8ef7627d89d63689c55f9c0b
Victor Ludwick <victor_ludwick@hotmail.com.br>
2019-01-20 17:44:11 -0200

    Pedido, EstadoPagamento e Pagamento

--- src/main/java/com/victorludwick/cursomc/CursomcApplication.java
+++ src/main/java/com/victorludwick/cursomc/CursomcApplication.java
@@ -1,5 +1,6 @@
 package com.victorludwick.cursomc;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,13 +13,20 @@ import com.victorludwick.cursomc.domain.Cidade;
 import com.victorludwick.cursomc.domain.Cliente;
 import com.victorludwick.cursomc.domain.Endereco;
 import com.victorludwick.cursomc.domain.Estado;
+import com.victorludwick.cursomc.domain.Pagamento;
+import com.victorludwick.cursomc.domain.PagamentoComBoleto;
+import com.victorludwick.cursomc.domain.PagamentoComCartao;
+import com.victorludwick.cursomc.domain.Pedido;
 import com.victorludwick.cursomc.domain.Produto;
+import com.victorludwick.cursomc.domain.enums.EstadoPagamento;
 import com.victorludwick.cursomc.domain.enums.TipoCliente;
 import com.victorludwick.cursomc.repositories.CategoriaRepository;
 import com.victorludwick.cursomc.repositories.CidadeRepository;
 import com.victorludwick.cursomc.repositories.ClienteRepository;
 import com.victorludwick.cursomc.repositories.EnderecoRepository;
 import com.victorludwick.cursomc.repositories.EstadoRepository;
+import com.victorludwick.cursomc.repositories.PagamentoRepository;
+import com.victorludwick.cursomc.repositories.PedidoRepository;
 import com.victorludwick.cursomc.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -36,6 +44,10 @@ public class CursomcApplication implements CommandLineRunner {
 	private ClienteRepository clienteRepository;
 	@Autowired
 	private EnderecoRepository enderecoRepository;
+	@Autowired
+	private PedidoRepository pedidoRepository;
+	@Autowired
+	private PagamentoRepository pagamentoRepository;
 
 	
 	public static void main(String[] args) {
@@ -86,9 +98,23 @@ public class CursomcApplication implements CommandLineRunner {
 		
 		clienteRepository.save(Arrays.asList(cli1));
 		enderecoRepository.save(Arrays.asList(e1, e2));
+		
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm");
+		
+		Pedido ped1 = new Pedido(null, sdf.parse("30/09/2017 10:32"), cli1, e1);
+		Pedido ped2 = new Pedido(null, sdf.parse("10/10/2017 19:35"), cli1, e2);
+		
+		Pagamento pagto1 = new PagamentoComCartao(null, EstadoPagamento.QUITADO, ped1, 6);
+		ped1.setPagamento(pagto1);
+		
+		Pagamento pagto2 = new PagamentoComBoleto(null, EstadoPagamento.PENDENTE, ped2, sdf.parse("20/10/2017 00:00"), null);
+		ped2.setPagamento(pagto2);
+		
+		cli1.getPedidos().addAll(Arrays.asList(ped1, ped2));
+		
+		pedidoRepository.save(Arrays.asList(ped1, ped2));
+		pagamentoRepository.save(Arrays.asList(pagto1, pagto2));
+		
 	}
-	
-	
-	
 }
 
