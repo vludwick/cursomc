e032e1a88716aea7e3ea5bf55a98b21416c6746b
Victor Ludwick <victor_ludwick@hotmail.com.br>
2019-01-24 16:07:19 -0200

    Endpoint /pedidos/{id} disponibilizado

--- src/main/java/com/victorludwick/cursomc/domain/ItemPedidoPK.java
+++ src/main/java/com/victorludwick/cursomc/domain/ItemPedidoPK.java
@@ -6,6 +6,7 @@ import javax.persistence.Embeddable;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 
+
 @Embeddable
 public class ItemPedidoPK implements Serializable{
 	private static final long serialVersionUID = 1L;
---
+++ src/main/java/com/victorludwick/cursomc/services/PedidoService.java
@@ -0,0 +1,25 @@
+package com.victorludwick.cursomc.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.victorludwick.cursomc.domain.Pedido;
+import com.victorludwick.cursomc.repositories.PedidoRepository;
+import com.victorludwick.cursomc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class PedidoService {
+
+	@Autowired
+	private PedidoRepository repo;
+
+	public Pedido buscar(Integer id) {
+		Pedido obj = repo.findOne(id);
+		if (obj == null) {
+			throw new ObjectNotFoundException(
+					"Objeto não encontrado! Id: " + id + ", Tipo " + Pedido.class.getName());
+		}
+		return obj;
+	}
+
+}
