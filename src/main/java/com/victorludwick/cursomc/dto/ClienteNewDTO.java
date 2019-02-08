df8d6401d35ecc90bb00fa9383b561a6feff1805
Victor Ludwick <victor_ludwick@hotmail.com.br>
2019-02-08 11:59:13 -0200

    Validacao customizada de CPF ou CNPJ na insercao de Cliente

--- src/main/java/com/victorludwick/cursomc/dto/ClienteNewDTO.java
+++ src/main/java/com/victorludwick/cursomc/dto/ClienteNewDTO.java
@@ -2,20 +2,40 @@ package com.victorludwick.cursomc.dto;
 
 import java.io.Serializable;
 
+import org.hibernate.validator.constraints.Email;
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.NotEmpty;
+
+import com.victorludwick.cursomc.services.validation.ClienteInsert;
+
+@ClienteInsert
 public class ClienteNewDTO implements Serializable{
 	private static final long serialVersionUID = 1L;
 
-	private Integer id;
+	@NotEmpty(message="Preenchimento obrigatório")
+	@Length(min=5, max=120,  message="O tamanho deve ser entre 5 e 120 caracteres")
 	private String nome;
+	
+	@NotEmpty(message="Preenchimento obrigatório")
+	@Email(message="Email inválido")
 	private String email;
+	
+	@NotEmpty(message="Preenchimento obrigatório")
 	private String cpfOuCnpj;
 	private Integer tipo;
+	
+	@NotEmpty(message="Preenchimento obrigatório")
 	private String logradouro;
+	
+	@NotEmpty(message="Preenchimento obrigatório")
 	private String numero;
 	private String complemento;
 	private String bairro;
+	
+	@NotEmpty(message="Preenchimento obrigatório")
 	private String cep;
 	
+	@NotEmpty(message="Preenchimento obrigatório")
 	private String telefone1;
 	private String telefone2;
 	private String telefone3;
@@ -24,14 +44,6 @@ public class ClienteNewDTO implements Serializable{
 	
 	public ClienteNewDTO() {
 		
-	}
-
-	public Integer getId() {
-		return id;
-	}
-
-	public void setId(Integer id) {
-		this.id = id;
 	}
 
 	public String getNome() {
