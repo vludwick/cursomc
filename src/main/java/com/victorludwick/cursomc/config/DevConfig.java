e6fef079756d26062c506c3923127ead22e64437
Victor Ludwick <victor_ludwick@hotmail.com.br>
2019-02-17 17:28:15 -0300

    Implementando SmtpEmailService com servidor do Google

--- src/main/java/com/victorludwick/cursomc/config/DevConfig.java
+++ src/main/java/com/victorludwick/cursomc/config/DevConfig.java
@@ -9,6 +9,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 import com.victorludwick.cursomc.services.DBService;
+import com.victorludwick.cursomc.services.EmailService;
+import com.victorludwick.cursomc.services.SmtpEmailService;
 
 @Configuration
 @Profile("dev")
@@ -30,4 +32,10 @@ public class DevConfig {
 		dbService.instantiateTestDatabase();
 		return true;
 	}
+	
+	@Bean 
+	public EmailService emailService() {
+		return new SmtpEmailService();
+	}
+	
 }
