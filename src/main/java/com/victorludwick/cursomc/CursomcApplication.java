d14bae03d9b89e4b1aad10fc2bf53762648b1c9c
Victor Ludwick <victor_ludwick@hotmail.com.br>
2019-02-24 13:53:15 -0300

    Salvando primeiro arquivo no S3

--- src/main/java/com/victorludwick/cursomc/CursomcApplication.java
+++ src/main/java/com/victorludwick/cursomc/CursomcApplication.java
@@ -1,13 +1,17 @@
 package com.victorludwick.cursomc;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.victorludwick.cursomc.services.S3Service;
+
 @SpringBootApplication
 public class CursomcApplication implements CommandLineRunner {
 	
-
+	@Autowired
+	private S3Service s3Service;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -16,7 +20,7 @@ public class CursomcApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		
-		
+		s3Service.uploadFile("C:\\Users\\ludwick\\Documents\\Fotos\\coala.jpg");
 	}
 }
 
