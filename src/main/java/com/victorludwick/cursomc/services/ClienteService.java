5812e919f548beefbcad9113add31d9aea2470f9
Victor Ludwick <victor_ludwick@hotmail.com.br>
2019-02-24 17:24:07 -0300

    Salvando a URL da imagem em Cliente

--- src/main/java/com/victorludwick/cursomc/services/ClienteService.java
+++ src/main/java/com/victorludwick/cursomc/services/ClienteService.java
@@ -114,7 +114,20 @@ public class ClienteService {
 	}
 
 	public URI uploadProfilePicture(MultipartFile multipartFile) {
-		return s3service.uploadFile(multipartFile);
+		
+		UserSS user = UserService.authenticated();
+		if (user == null) {
+			throw new AuthorizationException("Acesso negado");
+		}
+		
+		URI uri = s3service.uploadFile(multipartFile);
+		
+		Cliente cli = repo.findOne(user.getId());
+		cli.setImageUrl(uri.toString());
+		repo.save(cli);
+		
+		return uri;
+		
 	}
 	
 }
