591121343b5dc237aa81e5d3677cd7bd54cb35e9
Victor Ludwick <victor_ludwick@hotmail.com.br>
2019-02-27 12:17:33 -0300

    Endpoint para buscar Cliente por email

--- src/main/java/com/victorludwick/cursomc/services/ClienteService.java
+++ src/main/java/com/victorludwick/cursomc/services/ClienteService.java
@@ -95,6 +95,21 @@ public class ClienteService {
 		return repo.findAll();
 	}
 	
+	
+	public Cliente findByEmail(String email) {
+		
+		UserSS user = UserService.authenticated();
+		if (user == null || !user.hasRole(Perfil.ADMIN) && !email.equals(user.getUsername())) {
+			throw new AuthorizationException("Acesso negado");
+		}
+		
+		Cliente obj = repo.findByEmail(email);
+		if (obj == null) {
+			throw new ObjectNotFoundException("Ojeto não encontrado! Id: " + user.getId() + ", Tipo: " + Cliente.class.getName());
+		}
+		return obj;
+	}
+	
 	public Page<Cliente> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
 		PageRequest pageRequest = new PageRequest(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		return repo.findAll(pageRequest);
@@ -139,5 +154,5 @@ public class ClienteService {
 		
 		return s3service.uploadFile(imageService.getInputStream(jpgImage, "jpg"), fileName, "image");
 	}
-	
+
 }
